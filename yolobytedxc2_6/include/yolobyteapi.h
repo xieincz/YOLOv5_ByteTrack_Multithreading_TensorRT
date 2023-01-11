c4b5a00351e1c24dbb22cdc5711203580614381f
xieincz <39424496+xieincz@users.noreply.github.com>
2023-01-11 10:00:20 +0800

    Remove redundant code
    Remove the business code for a specific project

--- yolobytedxc2_6/include/yolobyteapi.h
+++ yolobytedxc2_6/include/yolobyteapi.h
@@ -9,10 +9,13 @@
 #include <algorithm>
 #include <chrono>
 #include <cmath>
+#include <condition_variable>
 #include <ctime>
+#include <deque>
 #include <fstream>
 #include <iostream>
 #include <map>
+#include <mutex>
 #include <numeric>
 #include <opencv2/highgui/highgui.hpp>
 #include <opencv2/opencv.hpp>
@@ -21,12 +24,9 @@
 #include <sstream>
 #include <string>
 #include <vector>
-#include <deque>
 #include <thread>
-#include <mutex>
-#include <condition_variable>
-#include <pthread.h>
-//#include <atomic>
+#include <vector>
+//#include <pthread.h>//for cpu affinity, Linux only
 
 #include "BYTETracker.h"
 #include "NvInfer.h"
@@ -48,19 +48,14 @@ using namespace nvinfer1;
 
 class YoloByteAPI {
 public:
-    YoloByteAPI(char* yolo_engine_path, const float& conf_thresh = 0.45, const int& frame_rate = 30, const int& track_buffer = 30, const float& track_thresh = 0.5, const float& high_thresh = 0.6, const float& match_thresh = 0.8,const float& nms_thresh=0.5);
+    YoloByteAPI(char* yolo_engine_path, const float& conf_thresh = 0.45, const int& frame_rate = 30, const int& track_buffer = 30, const float& track_thresh = 0.5, const float& high_thresh = 0.6, const float& match_thresh = 0.8, const float& nms_thresh = 0.5);
     ~YoloByteAPI();
-    //vector<vector<float>> detectOneImage(char* image_path);
-    int detectImageDirForModel(char* images_dir, char* output_txt_path);
-    int detectImageDirForModel(char* images_dir, char* output_txt_path, char* output_image_dir);
-    int detectImageDirForModelRenew(char* images_dir, char* output_txt_path);
-    int detectImageDirForModelRenew(char* images_dir, char* output_txt_path, char* output_image_dir);
 
-    int detectImageDirForAlgorithm(const char* video_path, const char* polygon_str, const int& frame_count_thresh,int skip_num);  // frame_count_thresh: args['fps'] * args['time_thresh']
-int detectImageDirForAlgorithmFast(const char* video_path, const char* polygon_str, const int& frame_count_thresh,int skip_num);
+    // frame_count_thresh: args['fps'] * args['time_thresh']
+    int detectImageDirForAlgorithmFast(const char* video_path, const char* polygon_str, const int& frame_count_thresh, int skip_num);
+
 private:
     void drawResultForModel(const cv::Mat& org_img, const vector<STrack>& output_stracks, const string& output_image_path) const;
-    void writeResultForModel(FILE* fp, const vector<STrack>& output_stracks, const int& frame_id) const;
     BYTETracker* tracker = nullptr;
     int read_files_in_dir(const char* p_dir_name, vector<string>& file_names);
     void* trt_engine = NULL;
@@ -74,7 +69,7 @@ private:
     vector<DetectRes> dets;
     bool check_crash(const int& px, const int& py, const vector<pair<int, int>>& polygon);
     bool check_crash(const int& px, const int& py, const vector<int>& polygon);
-    //for multi-thread
+    // for multi-thread
     deque<cv::Mat> frames_queue;
     deque<vector<DetectRes>> dets_queue;
     deque<vector<STrack>> stracks_queue;
@@ -82,8 +77,8 @@ private:
     condition_variable cv_frames, cv_detect, cv_tracks, cv_res;
     thread t_detect, t_track, t_res;
     bool end = false;
-    //atomic<bool> end;
-    bool read_end=false,detect_end = false, track_end = false, res_end = false;
+    // atomic<bool> end;
+    bool read_end = false, detect_end = false, track_end = false, res_end = false;
     void detect();
     void track();
     void res();
--- yolobytedxc2_6/src/yolobyteapi.i
+++ yolobytedxc2_6/src/yolobyteapi.i
@@ -1,29 +1,20 @@
-%module yolobyteapi 
-//%include "std_vector.i" 
+%module yolobyteapi
 %{
 #include "yolobyteapi.h"
 %}
-
-//%template(FloatVector) std::vector<float>;
-//%template(FloatVectorVector) std::vector<std::vector<float>>;
 
 using namespace std;
 
 class YoloByteAPI {
 public:
-    YoloByteAPI(char* yolo_engine_path, const float& conf_thresh = 0.45, const int& frame_rate = 30, const int& track_buffer = 30, const float& track_thresh = 0.5, const float& high_thresh = 0.6, const float& match_thresh = 0.8,const float& nms_thresh=0.5);
+    YoloByteAPI(char* yolo_engine_path, const float& conf_thresh = 0.45, const int& frame_rate = 30, const int& track_buffer = 30, const float& track_thresh = 0.5, const float& high_thresh = 0.6, const float& match_thresh = 0.8, const float& nms_thresh = 0.5);
     ~YoloByteAPI();
-    //vector<vector<float>> detectOneImage(char* image_path);
-    int detectImageDirForModel(char* images_dir, char* output_txt_path);
-    int detectImageDirForModel(char* images_dir, char* output_txt_path, char* output_image_dir);
-    int detectImageDirForModelRenew(char* images_dir, char* output_txt_path);
-    int detectImageDirForModelRenew(char* images_dir, char* output_txt_path, char* output_image_dir);
 
-    int detectImageDirForAlgorithm(const char* video_path, const char* polygon_str, const int& frame_count_thresh,int skip_num);  // frame_count_thresh: args['fps'] * args['time_thresh']
-    int detectImageDirForAlgorithmFast(const char* video_path, const char* polygon_str, const int& frame_count_thresh,int skip_num);
+    // frame_count_thresh: args['fps'] * args['time_thresh']
+    int detectImageDirForAlgorithmFast(const char* video_path, const char* polygon_str, const int& frame_count_thresh, int skip_num);
+
 private:
     void drawResultForModel(const cv::Mat& org_img, const vector<STrack>& output_stracks, const string& output_image_path) const;
-    void writeResultForModel(FILE* fp, const vector<STrack>& output_stracks, const int& frame_id) const;
     BYTETracker* tracker = nullptr;
     int read_files_in_dir(const char* p_dir_name, vector<string>& file_names);
     void* trt_engine = NULL;
@@ -37,15 +28,16 @@ private:
     vector<DetectRes> dets;
     bool check_crash(const int& px, const int& py, const vector<pair<int, int>>& polygon);
     bool check_crash(const int& px, const int& py, const vector<int>& polygon);
-    //for multi-thread
+    // for multi-thread
     deque<cv::Mat> frames_queue;
     deque<vector<DetectRes>> dets_queue;
     deque<vector<STrack>> stracks_queue;
-    mutex mtx_frames, mtx_dets, mtx_stracks, mtx_res;
-    condition_variable cv_frames, cv_dets, cv_stracks, cv_res;
+    mutex mtx_frames, mtx_dets, mtx_tracks, mtx_res;
+    condition_variable cv_frames, cv_detect, cv_tracks, cv_res;
     thread t_detect, t_track, t_res;
     bool end = false;
-    bool read_end=false,detect_end = false, track_end = false, res_end = false;
+    // atomic<bool> end;
+    bool read_end = false, detect_end = false, track_end = false, res_end = false;
     void detect();
     void track();
     void res();
