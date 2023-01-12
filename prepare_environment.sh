9efb666d88da9765ef97af0fef0ed62cadd00a1c
xieincz <39424496+xieincz@users.noreply.github.com>
2023-01-12 11:59:25 +0800

    update prepare_environment.sh

--- prepare_environment.sh
+++ prepare_environment.sh
@@ -36,13 +36,29 @@ sudo make install
 
 pkg-config --modversion opencv4
 
+
+wget https://developer.download.nvidia.com/compute/cuda/11.1.1/local_installers/cuda_11.1.1_455.32.00_linux.run
+sudo apt-get --purge remove "*cuda*" "*cublas*" "*cufft*" "*cufile*" "*curand*" \
+ "*cusolver*" "*cusparse*" "*gds-tools*" "*npp*" "*nvjpeg*" "nsight*" "*nvvm*" --allow-change-held-packages -y
+sudo apt-get autoremove
+sudo sh cuda_11.1.1_455.32.00_linux.run --silent  --toolkit --samples
+cat /var/log/cuda-installer.log
+sudo apt-get --purge remove cuda nvidia* libnvidia-*
+sudo dpkg -l | grep cuda- | awk '{print $2}' | xargs -n1 dpkg --purge
+sudo apt-get remove cuda-*
+sudo apt autoremove
+sudo apt-get -y install nvidia-driver-460
+#31
+#1
+cd /usr/local/cuda/samples/1_Utilities/deviceQuery
+rm -f deviceQuery
+sudo make
+./deviceQuery
+
+
 echo "install TensorRT 7.2.2.3 on home"
 sudo apt update
 sudo apt install -y nano
-
-#wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-nvrtc-11-1_11.1.105-1_amd64.deb
-#sudo dpkg -i cuda-nvrtc-11-1_11.1.105-1_amd64.deb
-#sudo ln -s /usr/local/cuda-11.1/targets/x86_64-linux/lib/libnvrtc.so.11.1 /usr/local/cuda/lib64/libnvrtc.so.11.1
 
 cd /home
 echo "be careful! The package installed here is only applicable to Ubuntu 18.04 cuda 11.1 & 11.2"
@@ -54,16 +70,16 @@ tar -xzvf TensorRT7.2.2.3Ubuntu1804cuda11.1cudnn8.0.tar.gz
 
 sudo echo -e "\nexport TENSORRT_ROOT=/home/TensorRT-7.2.2.3\n
 export LD_LIBRARY_PATH=/home/TensorRT-7.2.2.3/lib:$LD_LIBRARY_PATH\n
-export CUDA_INSTALL_DIR=/usr/local/cuda-11\n
-export CUDNN_INSTALL_DIR=/usr/local/cuda-11\n
+export CUDA_INSTALL_DIR=/usr/local/cuda\n
+export CUDNN_INSTALL_DIR=/usr/local/cuda\n
 export LD_LIBRARY_PATH=/usr/local/cuda-11.1/targets/x86_64-linux/lib:$LD_LIBARARY_PATH" >> ~/.bashrc
 
 source ~/.bashrc
 
 sudo echo -e "\nexport TENSORRT_ROOT=/home/TensorRT-7.2.2.3\n
 export LD_LIBRARY_PATH=/home/TensorRT-7.2.2.3/lib:$LD_LIBRARY_PATH\n
-export CUDA_INSTALL_DIR=/usr/local/cuda-11\n
-export CUDNN_INSTALL_DIR=/usr/local/cuda-11\n
+export CUDA_INSTALL_DIR=/usr/local/cuda\n
+export CUDNN_INSTALL_DIR=/usr/local/cuda\n
 export LD_LIBRARY_PATH=/usr/local/cuda-11.1/targets/x86_64-linux/lib:$LD_LIBARARY_PATH" >> /etc/profile
 
 source /etc/profile
@@ -73,6 +89,14 @@ sudo pip3 install tensorrt-7.2.2.3-cp38-none-linux_x86_64.whl
 cd ..
 pip install uff/uff-0.6.9-py2.py3-none-any.whl
 pip install graphsurgeon/graphsurgeon-0.4.5-py2.py3-none-any.whl
+
+cd /home/TensorRT-7.2.2.3/data/mnist
+python download_pgms.py
+
+cd /home/TensorRT-7.2.2.3/samples/sampleMNIST
+make
+cd ../../bin
+./sample_mnist
 
 echo "install other packages"
 sudo apt update
@@ -91,8 +115,37 @@ cd ..
 cd ..
 
 cd yolobytedxc2_6
+
+rm -rf CMakeFiles CMakeCache.txt Makefile cmake_install.cmake yolobyteapi.py libyolov5_trt.so
+
 cmake ./ \
 -DPYTHON_INCLUDE_DIR=$(python -c "from distutils.sysconfig import get_python_inc; print(get_python_inc())")  \
 -DPYTHON_LIBRARY=$(python -c "import distutils.sysconfig as sysconfig; print(sysconfig.get_config_var('LIBDIR'))")
 
 make 
+
+
+echo "export tensorrt model"
+echo -e "\nAttention! This is only design for online coding evnerviment.\n"
+echo -e "\nIf you want to use it in local, please change the path in this file.\n"
+
+model_dir=/content/YOLOv5_ByteTrack_Multithreading_TensorRT/cppmodels/
+src_dir=/content/YOLOv5_ByteTrack_Multithreading_TensorRT/
+#最后面的斜杠不能少
+
+model_type=m6
+#可以是 n6 s6 m6 l6 x6之一
+
+model_name=yolov5${model_type}
+yolov5_cpp=yolov5_cpp_6
+############################################################################################################
+#/project/ev_sdk/model/yolov5l.pt -> /project/ev_sdk/model/yolov5l.engine
+cp ${src_dir}${yolov5_cpp}/gen_wts.py ${src_dir}yolov5/
+echo -e "\nConverting ${model_dir}${model_name}.pt to ${model_dir}${model_name}.wts\n"
+python ${src_dir}yolov5/gen_wts.py -w ${model_dir}${model_name}.pt -o ${model_dir}${model_name}.wts
+# update CLASS_NUM in yololayer.h if your model is trained on custom dataset
+echo -e "\nAttention! Please update CLASS_NUM in ${src_dir}${yolov5_cpp}/yololayer.h if your model is trained on custom dataset.\n"
+echo -e "\nConverting ${model_dir}${model_name}.wts to ${model_dir}${model_name}.engine\n"
+${src_dir}${yolov5_cpp}/build/yolov5 -s ${model_dir}${model_name}.wts ${model_dir}${model_name}.engine ${model_type}
+#rm ${model_dir}${model_name}.wts
+echo -e "\nDone.\n"
